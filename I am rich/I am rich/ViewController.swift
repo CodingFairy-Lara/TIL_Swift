3be1a55597cb05632de8511ee786cc2028d117cd
Hera J <97158923+CodingFairy-Lara@users.noreply.github.com>
2023-08-01 23:43:41 +0900

    TIL_Swift
    overview of Xcode

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Hera on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
