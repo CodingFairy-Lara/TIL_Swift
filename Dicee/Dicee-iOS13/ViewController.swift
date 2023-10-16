a5a9494c29a05fee9e5a7b65d16a945b34ce93a5
Hera Jang <97158923+codingfairy-lara@users.noreply.github.com>
2023-10-16 22:28:02 +0900

    TIL Swift
    TIL Swift

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -14,28 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
     
     @IBAction func RollButtonPressed(_ sender: UIButton) {
-        print("leftDiceNumber at beginning = \(leftDiceNumber)")
-        print("rightDiceNumber at beginning = \(rightDiceNumber)")
+
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
         
-        diceImageView1.image =  [ UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix") ][leftDiceNumber]
-        diceImageView2.image =  [ UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix") ][rightDiceNumber]
+//        print("leftDiceNumber at begining = \(leftDiceNumber)")
+
+//        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+//        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
         
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
-        
-        print("leftDiceNumber at the end = \(leftDiceNumber)")
-        print("rightDiceNumber at the end = \(rightDiceNumber)")
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1
+//        print("leftDiceNumber at the end = \(leftDiceNumber)")
+//        print(Int.random(in: 1...10))
         
     }
-    
 
 }
 
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,41 @@
-// This is comment
 
-// print("Hello World")
+var a = 5
+var b = 8
 
-/*
- I can write long comment here
- */
+var temp = a
+a = b
+b = temp
 
- print("Hello \(2+3) World")
- print("The result of 5+3 is \(5+3)")
+print("The value of a is \(a)")
+print("The value of a is \(b)")
+
+
+var numbers = [45,73, 195, 52]
+let computedNumbers =  [ numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[0]*numbers[3] ]
+                        
+print(computedNumbers)
+
+a = 10
+print(a)
+
+let c = 5
+print(c)
+// c = 1
+
+let pi = 3.14
+print(pi)
+
+var randomNumber = Int.random(in: 1...5)
+print(randomNumber)
+
+randomNumber = Int.random(in: 1 ..< 5)
+print(randomNumber)
+
+var randomNumber2 = Float.random(in: 1 ... 5)
+print(randomNumber2)
+
+
+
+
+
+
